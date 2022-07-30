cbc9b42bea66d10f19ff8dcd270a2343ee6c62d0
Ismael Grando <ismaelrossonigrando@gmail.com>
2022-07-29 22:10:26 -0300

    A

---
+++ Configurar Git.bat
@@ -0,0 +1,6 @@
+git config --global user.name "IsmaelGrando"
+git config --global user.email "ismaelrossonigrando@gmail.com"
+git init --initial-branch=main
+git remote add origin https://github.com/IsmaelGrando/Site-Noticias-Cidade.git
+
+timeout 200
